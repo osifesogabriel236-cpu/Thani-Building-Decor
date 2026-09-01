e3970923954482d19683cfa05d83ea63fb24b010
osifesogabriel236-cpu <osifesogabriel236@gmail.com>
2026-09-01 09:39:13 +0100

    supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://gzcvxpjivtwkfpmqtfnk.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://gzcvxpjivtwkfpmqtfnk.supabase.co";
 ‎const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_KQpXv0hgWZnABIzFRmuGxg_HEaRC4hk";
